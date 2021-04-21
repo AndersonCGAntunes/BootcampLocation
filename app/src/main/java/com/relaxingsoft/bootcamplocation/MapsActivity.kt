8b33ea7e72765fe56dcf70b7a7a893b2cc786b31
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-04-21 11:17:12 -0300

    Condições avançadas de mapa e conclusão da aula

--- app/src/main/java/com/relaxingsoft/bootcamplocation/MapsActivity.kt
+++ app/src/main/java/com/relaxingsoft/bootcamplocation/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.relaxingsoft.bootcamplocation
 
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
@@ -7,8 +8,8 @@ import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -26,8 +27,13 @@ class MapsActivity : AppCompatActivity(),
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
 
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHECK_SETTINGS = 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,17 +45,18 @@ class MapsActivity : AppCompatActivity(),
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient( this )
+
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+                placeMarkerOnMap( LatLng( lastLocation.latitude, lastLocation.longitude ) )
+            }
+        }
+        createLocationRequest()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
@@ -107,6 +114,58 @@ class MapsActivity : AppCompatActivity(),
         val country = addresses[ 0 ].countryName
         val postalCode = addresses[ 0 ].postalCode
         return address
+    }
+
+    private fun startLocationUpdates() {
+        if ( ActivityCompat.checkSelfPermission( this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION )
+        != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this,
+            arrayOf( android.Manifest.permission.ACCESS_FINE_LOCATION ),
+            LOCATION_PERMISSION_REQUEST_CODE )
+            return
+        }
+
+        fusedLocationClient.requestLocationUpdates( locationRequest, locationCallback, null )
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest( locationRequest )
+
+        val client = LocationServices.getSettingsClient( this )
+        var task = client.checkLocationSettings( builder.build() )
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+
+        task.addOnFailureListener { e ->
+            if ( e is ResolvableApiException ) {
+                try {
+                    e.startResolutionForResult( this@MapsActivity,
+                    REQUEST_CHECK_SETTINGS )
+                } catch ( sendEx: IntentSender.SendIntentException ) {  }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates( locationCallback )
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if ( !locationUpdateState ) {
+            startLocationUpdates()
+        }
     }
 
     override fun onMarkerClick(p0: Marker?) = false
